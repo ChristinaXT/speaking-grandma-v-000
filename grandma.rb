7e25dd5f7943539990386932df203a1f888e8614
Christina Williams <christinaxtwilliams@gmail.com>
2018-09-25 00:51:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,8 +18,7 @@ def speak_to_grandma(phrase)
     return 'NO, NOT SINCE 1938!'
   elseif phrase.uppercase
        return 'NO, NOT SINCE 1938!'
-   else 
-       return 'HUH?! SPEAK UP, SONNY!' 
+   end 
     if  phrase =='I LOVE YOU GRANDMA!'
       return 'I LOVE YOU TOO PUMPKIN!' 
   end
